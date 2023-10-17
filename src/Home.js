54ffbe360bccae09bdf9519b85f0381afe32c1ec
Justine Chu <justinechu@Songhyeons-MacBook-Pro1.local>
2023-10-17 11:45:33 -0500

    updated

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+function Home(){
+    // const continents = ["North America", "South America", "Europe"];
+    // const navigate = useNavigate();
+    return (
+        <div className="home">
+        </div>
+    );
+}
+
+export default Home;
