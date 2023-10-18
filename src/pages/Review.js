716afb240aa4adf7e5719ed8197cd2680dad9154
Justine Chu <justinegeechu333@gmail.com>
2023-10-18 10:49:57 -0500

    fix the bug

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,35 +1,35 @@
-// import React, { useEffect, useState } from "react";
-// import { Container, Grid, Image } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Container, Grid, Image } from "semantic-ui-react";
 
-// function ReviewCard({ review }) {
+function ReviewCard({ review }) {
+  return (
+    <Grid>
+      <Grid.Column width={4}>
+        <Image src="/images/wireframe/paragraph.png" />
+      </Grid.Column>
+      <Grid.Column width={9}>
+        <Container textAlign="left">Left Aligned</Container>
+      </Grid.Column>
+    </Grid>
+  );
+}
 
-//   return (
-//     <Grid>
-//       <Grid.Column width={4}>
-//         <Image src="/images/wireframe/paragraph.png" />
-//       </Grid.Column>
-//       <Grid.Column width={9}>
-//         <Container textAlign='left'>Left Aligned</Container>
-//       </Grid.Column>
-//     </Grid>
-//   );
-// }
+function ReviewList({ dests }) {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3010/reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  });
+  return (
+    <Grid>
+      {reviews.map((review) => {
+        return <ReviewCard key={review.id} review={review} />;
+      })}
+    </Grid>
+  );
+}
 
-// function ReviewList({dests}) {
-//     const [reviews, setReviews] = useState([]);
-//     useEffect(()=>{
-//         fetch('http://localhost:3010/reviews').then(res=>res.json())
-//             .then(data=>{
-//                 setReviews(data);
-//             })
-//     })
-//   return (
-//     <Grid.Group>
-//       {reviews.map((review) => {
-//         return <ReviewCard key={reviews.id} review={review} />;
-//       })}
-//     </Grid.Group>
-//   );
-// }
-
-// export default ReviewList;
+export default ReviewList;
